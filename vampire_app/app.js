986b066774b4cbcbe8648f08ec6d4e02fa1f1203
Sandra Abernathy <sandraabernathy@Sandras-MacBook-Pro.local>
2019-07-18 19:27:32 -0600

    Commit 2 - added data into vampire collection

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -16,8 +16,83 @@ const vampireData = require('./populateVampires')
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-
+// Vampire.collection.insertMany(vampireData,(err,data) => {
+//     console.log('added provided vampire data')
+//     mongoose.connection.close();
+// });
 // ### Add some new vampire data
+// Vampire.create(
+//     {name: 'Jean-Claude',
+//     hair_color: 'midnight blue',
+//     eye_color: 'ocean blue',
+//     dob: "December 31, 1860",
+//     loves: ["Anita","Blood"],
+//     location: "Saint Louis, Missiouri",
+//     gender: 'm',
+//     victims: 698,
+//     }, (err, createdVampire) => {
+//         if(err) {
+//             console.log(err);
+//         }else {
+//             console.log(createdVampire);
+//         }
+//     }
+// );
+
+// Vampire.create(
+//     {name: 'Ash',
+//     hair_color: 'platinum blond',
+//     eye_color: 'sky blue',
+//     dob: "August 12, 1905",
+//     loves: ["Jean-Claude","Himself"],
+//     location: "Saint Louis, Missiouri",
+//     gender: 'm',
+//     victims: 486,
+//     }, (err, createdVampire) => {
+//         if(err) {
+//             console.log(err);
+//         }else {
+//             console.log(createdVampire);
+//         }
+//     }
+// );
+
+// Vampire.create(
+//     {name: 'Marmee Noir',
+//     hair_color: 'Black',
+//     eye_color: 'Black',
+//     dob: "-12000",
+//     loves: ["Death","Power"],
+//     location: "Unknown",
+//     gender: 'f',
+//     victims: 1000000000,
+//     }, (err, createdVampire) => {
+//         if(err) {
+//             console.log(err);
+//         }else {
+//             console.log(createdVampire);
+//         }
+//     }
+// );
+
+// Vampire.create(
+//     {name: 'Belle Morte',
+//     hair_color: 'Blonde',
+//     eye_color: 'royal blue',
+//     dob: "June 7, 1649",
+//     loves: ["Power","Herself"],
+//     location: "Europe",
+//     gender: 'f',
+//     victims: 4879,
+//     }, (err, createdVampire) => {
+//         if(err) {
+//             console.log(err);
+//         }else {
+//             console.log(createdVampire);
+//         }
+//     }
+// );
+
 
 /////////////////////////////////////////////////
 // ## QUERYING
